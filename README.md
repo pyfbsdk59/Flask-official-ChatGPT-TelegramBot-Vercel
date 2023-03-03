f70f41e7e40a8144c703a52b40fe7b7e57046898
twstock168service <twstock168.service@gmail.com>
2023-03-03 09:09:36 +0800

    1c

---
+++ README.md
@@ -0,0 +1,88 @@
+# Flask-ChatGPT-TelegramBot-Vercel
+# 一個Flask ChatGPT TelegramBot快速建置於平台Vercel。
+
+## [TelegramBot Render版本部屬](https://github.com/pyfbsdk59/Flask-ChatGPT-TelegramBot-Render)
+
+<div align="center">
+  <img src="demo/link.png" width="700"/>
+</div>
+
+## [LineBot Django Vercel版本部屬](https://github.com/pyfbsdk59/Django-ChatGPT-linebot-Vercel)
+
+<div align="center">
+  <img src="demo/link2.png" width="700"/>
+</div>
+
+## [TelegramBot Golang Render版本部屬](https://github.com/pyfbsdk59/Golang-ChatGPT-TelegramBot-Render)
+
+<div align="center">
+  <img src="demo/link3.png" width="700"/>
+</div>
+
+<div align="center">
+  <img src="demo/link3a.png" width="600"/>
+</div>
+
+## [LineBot Golang Render版本部屬](https://github.com/pyfbsdk59/Golang-ChatGPT-linebot-Render)
+
+<div align="center">
+  <img src="demo/link4.png" width="700"/>
+</div>
+
+### [English](https://github.com/pyfbsdk59/Flask-ChatGPT-TelegramBot-Vercel/blob/main/README_en.md)
+### [日本語](https://github.com/pyfbsdk59/Flask-ChatGPT-TelegramBot-Vercel/blob/main/README_jp.md)
+
+
+
+
+#### 0. 程式猿影音教學參考。請支持且訂閱加按讚感謝他的辛勞。
+
+https://www.youtube.com/watch?v=eKKEa6NhCd0
+
+
+<div align="center">
+  <img src="demo/demo0.png" width="800"/>
+</div>
+
+#### 1. 本專案參考了以下前輩的方案改成製作，只針對剛學習Flask的朋友來佈置TelegramBot在Vercel上：
+
+https://github.com/howarder3/GPT-Linebot-python-flask-on-vercel<br><br>
+https://github.com/zaoldyeck/telegram-innovation-chatbot/tree/basic
+
+#### 2. 本專案因部屬在Vercel上，所以程式碼和Docker版本不同，也必須使用Flask和設定webhook。設定webhook請參考。
+
+https://zaoldyeck.medium.com/%E6%89%8B%E6%8A%8A%E6%89%8B%E6%95%99%E4%BD%A0%E6%80%8E%E9%BA%BC%E6%89%93%E9%80%A0-telegram-bot-a7b539c3402a
+
+
+#### 3. 註冊Vercel帳號後，import已經fork過來的本專案。
+
+<div align="center">
+  <img src="demo/demo1.png" width="600"/>
+</div>
+
+#### 4. 必須在Vercel的Environment Variables設定兩個環境變數，分別是OPENAI_API_KEY和TELEGRAM_BOT_TOKEN。順便記下Vercel給的網址，可以自己縮短一些。
+
+<div align="center">
+  <img src="demo/demo2.png" width="600"/>
+</div>
+
+#### 5. 打開瀏覽器，輸入以下網址，設定webhook為部屬完Vercel的最後步驟，格式為：https://api.telegram.org/bot{$token}/setWebhook?url={$webhook_url}。
+
+##### 故實際範例就像以下範例（非直接複製使用，請改用自己的telegram token和Vercel專案的URL）：
+
+
+https://api.telegram.org/bot606248605:AAGv_TOJdNNMc_v3toHK_X6M-dev_1tG-JA/setWebhook?url=https://xxx.vercel.app/callback
+
+
+#### 6. 成功後會顯示以下文字：
+
+{
+  ok: true,
+  result: true,
+  description: "Webhook was set"
+}
+------
+### 創建Telegram機器人和取得token，請參考： 
+https://ithelp.ithome.com.tw/articles/10245264<br><br>
+https://tcsky.cc/tips-01-telegram-chatbot/
+
---
+++ main.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+import telegram, os
+from flask import Flask, request
+from telegram.ext import Dispatcher, MessageHandler, Filters
+
+
+
+#################
+import openai
+	
+openai.api_key = os.getenv("OPENAI_API_KEY") 
+
+
+chat_language = os.getenv("INIT_LANGUAGE", default = "zh") #amend here to change your preset language
+	
+conversation = []
+
+class ChatGPT:  
+    
+
+    def __init__(self):
+        
+        self.messages = conversation
+        self.model = os.getenv("OPENAI_MODEL", default = "gpt-3.5-turbo")
+        self.temperature = float(os.getenv("OPENAI_TEMPERATURE", default = 2))
+        self.max_tokens = int(os.getenv("OPENAI_MAX_TOKENS", default = 240))
+
+
+    def get_response(self, user_input):
+        conversation.append({"role": "user", "content": user_input})
+        
+
+        response = openai.ChatCompletion.create(
+	            model=self.model,
+                messages = self.conversation
+	            temperature=self.temperature,
+	            frequency_penalty=self.frequency_penalty,
+	            presence_penalty=self.presence_penalty,
+	            max_tokens=self.max_tokens
+                )
+
+        conversation.append({"role": "assistant", "content": response['choices'][0]['message']['content']})
+        
+        print("AI回答內容：")        
+        print(response['choices'][0]['message']['content'].strip())
+
+        #print("AI原始回覆資料內容：")      
+        #print(response)
+        
+        return response['choices'][0]['message']['content'].strip()
+	
+
+
+
+
+
+
+
+#####################
+
+telegram_bot_token = str(os.getenv("TELEGRAM_BOT_TOKEN"))
+
+
+
+# Load data from config.ini file
+#config = configparser.ConfigParser()
+#config.read('config.ini')
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Initial Flask app
+app = Flask(__name__)
+
+# Initial bot by Telegram access token
+bot = telegram.Bot(token=telegram_bot_token)
+
+
+
+@app.route('/callback', methods=['POST'])
+def webhook_handler():
+    """Set route /hook with POST method will trigger this method."""
+    if request.method == "POST":
+        update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+        # Update dispatcher process that handler to process this message
+        dispatcher.process_update(update)
+    return 'ok'
+
+
+def reply_handler(bot, update):
+    """Reply message."""
+    #text = update.message.text
+    #update.message.reply_text(text)
+    chatgpt = ChatGPT()        
+    
+                                            #update.message.text 人類的問題 the question humans asked
+    ai_reply_response = chatgpt.get_response(update.message.text) #ChatGPT產生的回答 the answers that ChatGPT gave
+    
+    update.message.reply_text(ai_reply_response) #用AI的文字回傳 reply the text that AI made
+
+# New a dispatcher for bot
+dispatcher = Dispatcher(bot, None)
+
+# Add handler for handling message, there are many kinds of message. For this handler, it particular handle text
+# message.
+dispatcher.add_handler(MessageHandler(Filters.text, reply_handler))
+
+if __name__ == "__main__":
+    # Running server
+    app.run(debug=True)
